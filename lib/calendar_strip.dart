f9b422688ee0223c7e802c9f260eecaa62476e36
Siddharth <siddharthsakre@gmail.com>
2019-10-31 15:24:34 +0530

    Basic implementation of calendar strip is done

--- lib/calendar_strip.dart
+++ lib/calendar_strip.dart
@@ -1,7 +1,122 @@
 library calendar_strip;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import './date-utils.dart';
+
+class CalendarStrip extends StatefulWidget {
+  // This widget is the root of your application.
+  State<CalendarStrip> createState() => CalendarStripState();
+}
+
+class CalendarStripState extends State<CalendarStrip> {
+  DateTime currentDate = DateTime.now();
+  DateTime selectedDate = DateTime.now();
+  String monthLabel;
+  bool inBetweenMonths = false;
+  int rowStartingDay;
+  DateTime lastDayOfMonth;
+  List monthLabels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  CalendarStripState() {
+    lastDayOfMonth = DateUtils.getLastDayOfMonth(currentDate);
+  }
+
+  int getLastDayOfMonth(rowStartingDay) {
+    return DateUtils.getLastDayOfMonth(currentDate.add(Duration(days: rowStartingDay))).day;
+  }
+
+  String getMonthLabel() {
+    int startingDayMonth, endingDayMonth;
+    int rowEndingDay = rowStartingDay + 7;
+    String label = "";
+    print(rowStartingDay);
+    if (rowStartingDay > 0) {
+      startingDayMonth = currentDate.add(Duration(days: rowStartingDay.abs())).month;
+    } else {
+      startingDayMonth = currentDate.subtract(Duration(days: rowStartingDay.abs())).month;
+    }
+
+    if (rowEndingDay > 0) {
+      endingDayMonth = currentDate.add(Duration(days: rowEndingDay.abs())).month;
+    } else {
+      endingDayMonth = currentDate.subtract(Duration(days: rowEndingDay.abs())).month;
+    }
+
+    print("$startingDayMonth $endingDayMonth");
+    if (startingDayMonth == endingDayMonth) {
+      label = monthLabels[startingDayMonth - 1];
+    } else {
+      label = "${monthLabels[startingDayMonth - 1]} / ${monthLabels[endingDayMonth - 1]}";
+    }
+    return label;
+  }
+
+  onPrevRow() {
+    setState(() {
+      rowStartingDay = rowStartingDay - 7;
+    });
+  }
+
+  onNextRow() {
+    setState(() {
+      rowStartingDay = rowStartingDay + 7;
+    });
+  }
+
+  buildDateRow() {
+    List<Widget> currentWeekRow = [];
+    rowStartingDay = rowStartingDay != null ? rowStartingDay : currentDate.day - currentDate.weekday + 1;
+    monthLabel = getMonthLabel();
+    for (var eachDay = rowStartingDay; eachDay < rowStartingDay + 7; eachDay++) {
+      if (eachDay > currentDate.day) {
+        currentWeekRow.add(dateTileBuilder(currentDate.add(Duration(days: eachDay))));
+      } else {
+        currentWeekRow.add(dateTileBuilder(currentDate.subtract(Duration(days: currentDate.day - eachDay))));
+      }
+    }
+    return Column(children: [
+      Container(child: Text(monthLabel)),
+      Container(
+        child: Row(children: [
+          GestureDetector(child: Container(child: Icon(Icons.chevron_left, size: 40)), onTap: onPrevRow),
+          Expanded(child: Row(children: currentWeekRow)),
+          GestureDetector(child: Container(child: Icon(Icons.chevron_right, size: 40)), onTap: onNextRow),
+        ]),
+      )
+    ]);
+  }
+
+  Widget dateTileBuilder(DateTime date) {
+    return Expanded(
+      child: Container(
+          alignment: Alignment.center,
+          child: Text(
+            date.day.toString(),
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w800),
+          )),
+    );
+  }
+
+  build(BuildContext context) {
+    return Container(
+      height: 80,
+      decoration: BoxDecoration(border: Border.all(width: 1, color: Colors.red)),
+      child: buildDateRow(),
+    );
+  }
 }
---
+++ lib/date-utils.dart
@@ -0,0 +1,132 @@
+class DateUtils {
+  static DateTime toMidnight(DateTime dateTime) {
+    return DateTime(dateTime.year, dateTime.month, dateTime.day);
+  }
+
+  static bool isWeekend(DateTime date) {
+    return date.weekday == DateTime.saturday || date.weekday == DateTime.sunday;
+  }
+
+  static bool isToday(DateTime date) {
+    var now = DateTime.now();
+    return date.day == now.day && date.month == now.month && date.year == now.year;
+  }
+
+  static bool isPastDay(DateTime date) {
+    var today = toMidnight(DateTime.now());
+    return date.isBefore(today);
+  }
+
+  static DateTime addDaysToDate(DateTime date, int days) {
+    DateTime newDate = date.add(Duration(days: days));
+
+    if (date.hour != newDate.hour) {
+      var hoursDifference = date.hour - newDate.hour;
+
+      if (hoursDifference <= 3 && hoursDifference >= -3) {
+        newDate = newDate.add(Duration(hours: hoursDifference));
+      } else if (hoursDifference <= -21) {
+        newDate = newDate.add(Duration(hours: 24 + hoursDifference));
+      } else if (hoursDifference >= 21) {
+        newDate = newDate.add(Duration(hours: hoursDifference - 24));
+      }
+    }
+    return newDate;
+  }
+
+  static bool isSpecialPastDay(DateTime date) {
+    return isPastDay(date) || (isToday(date) && DateTime.now().hour >= 12);
+  }
+
+  static DateTime getFirstDayOfCurrentMonth() {
+    var dateTime = DateTime.now();
+    dateTime = getFirstDayOfMonth(dateTime);
+    return dateTime;
+  }
+
+  static DateTime getFirstDayOfNextMonth() {
+    var dateTime = getFirstDayOfCurrentMonth();
+    dateTime = addDaysToDate(dateTime, 31);
+    dateTime = getFirstDayOfMonth(dateTime);
+    return dateTime;
+  }
+
+  static DateTime getLastDayOfCurrentMonth() {
+    return getLastDayOfMonth(DateTime.now());
+  }
+
+  static DateTime getLastDayOfNextMonth() {
+    return getLastDayOfMonth(getFirstDayOfNextMonth());
+  }
+
+  static DateTime addMonths(DateTime fromMonth, int months) {
+    DateTime firstDayOfCurrentMonth = fromMonth;
+    for (int i = 0; i < months; i++) {
+      firstDayOfCurrentMonth = getLastDayOfMonth(firstDayOfCurrentMonth).add(Duration(days: 1));
+    }
+
+    return firstDayOfCurrentMonth;
+  }
+
+  static DateTime getFirstDayOfMonth(DateTime month) {
+    return DateTime(month.year, month.month);
+  }
+
+  static DateTime getLastDayOfMonth(DateTime month) {
+    DateTime firstDayOfMonth = DateTime(month.year, month.month);
+    DateTime nextMonth = firstDayOfMonth.add(Duration(days: 32));
+    DateTime firstDayOfNextMonth = DateTime(nextMonth.year, nextMonth.month);
+    return firstDayOfNextMonth.subtract(Duration(days: 1));
+  }
+
+  static bool isSameDay(DateTime date1, DateTime date2) {
+    return date1.day == date2.day && date1.month == date2.month && date1.year == date2.year;
+  }
+
+  static bool isCurrentMonth(DateTime date) {
+    var now = DateTime.now();
+    return date.month == now.month && date.year == now.year;
+  }
+
+  static int calculateMaxWeeksNumberMonthly(DateTime startDate, DateTime endDate) {
+    int monthsNumber = calculateMonthsDifference(startDate, endDate);
+
+    List<int> weeksNumbersMonthly = List();
+
+    if (monthsNumber == 0) {
+      return calculateWeeksNumber(startDate, endDate);
+    } else {
+      weeksNumbersMonthly.add(calculateWeeksNumber(startDate, getLastDayOfMonth(startDate)));
+
+      DateTime firstDateOfMonth = getFirstDayOfMonth(startDate);
+      for (int i = 1; i <= monthsNumber - 2; i++) {
+        firstDateOfMonth = firstDateOfMonth.add(Duration(days: 31));
+        weeksNumbersMonthly.add(calculateWeeksNumber(firstDateOfMonth, getLastDayOfMonth(firstDateOfMonth)));
+      }
+
+      weeksNumbersMonthly.add(calculateWeeksNumber(getFirstDayOfMonth(endDate), endDate));
+
+      weeksNumbersMonthly.sort((a, b) => b.compareTo(a));
+      return weeksNumbersMonthly[0];
+    }
+  }
+
+  static int calculateMonthsDifference(DateTime startDate, DateTime endDate) {
+    var yearsDifference = endDate.year - startDate.year;
+    return 12 * yearsDifference + endDate.month - startDate.month;
+  }
+
+  static int calculateWeeksNumber(DateTime monthStartDate, DateTime monthEndDate) {
+    int rowsNumber = 1;
+
+    DateTime currentDay = monthStartDate;
+    while (currentDay.isBefore(monthEndDate)) {
+      currentDay = currentDay.add(Duration(days: 1));
+      if (currentDay.weekday == DateTime.monday) {
+        rowsNumber += 1;
+      }
+    }
+
+    return rowsNumber;
+  }
+}
